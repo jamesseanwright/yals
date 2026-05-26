92e801bbec985883969e2d6073b51429725f33cc
James Wright <jsw@mahoganyteeth.com>
2026-05-26 22:18:50 +0100

    Refactors create URL validation

--- Yaurs/UrlsMapGroup.cs
+++ Yaurs/UrlsMapGroup.cs
@@ -13,8 +13,7 @@ static class UrlsMapGroup
 
     internal static async Task<Results<Created<Url>, ValidationProblem>> HandleCreate(IUrlService urlService, CreateUrlDto createUrlDto)
     {
-        if (!createUrlDto.TargetUri.IsAbsoluteUri ||
-            (createUrlDto.TargetUri.Scheme != Uri.UriSchemeHttp && createUrlDto.TargetUri.Scheme != Uri.UriSchemeHttps))
+        if (!IsRedirectableUri(createUrlDto.TargetUri))
         {
             return TypedResults.ValidationProblem(new Dictionary<string, string[]>
             {
@@ -26,4 +25,7 @@ static class UrlsMapGroup
 
         return TypedResults.Created($"http://TODO", createdUrl);
     }
+
+    private static bool IsRedirectableUri(Uri targetUri) => targetUri.IsAbsoluteUri &&
+            (targetUri.Scheme == Uri.UriSchemeHttp || targetUri.Scheme == Uri.UriSchemeHttps);
 }
